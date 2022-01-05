c85ccd96f184f262b32b6073afb4e032d46f0ed5
chengyinfeng <itxc49001@gmail.com>
2022-01-06 01:00:13 +0800

    每日leetcode

--- src/leetcode/editor/cn/FirstUniqueCharacterInAString.java
+++ src/leetcode/editor/cn/FirstUniqueCharacterInAString.java
@@ -21,37 +21,31 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 //Java：字符串中的第一个唯一字符
-public class FirstUniqueCharacterInAString{
+public class FirstUniqueCharacterInAString {
     public static void main(String[] args) {
         Solution solution = new FirstUniqueCharacterInAString().new Solution();
         // TO TEST
         System.out.println(solution.firstUniqChar("loveleetcode"));
     }
-    
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int firstUniqChar(String s) {
-        // 找到不重复的字符返回其索引。不存在则返回-1
-        // 使用hashmap记录字符出现的次数，如果次数为一直接返回
-        char[] chars = s.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < chars.length; i++) {
-            if (map.containsKey(chars[i])) {
-                map.put(chars[i], map.get(chars[i]) + 1);
-            } else {
-                map.put(chars[i], 1);
+    class Solution {
+        public int firstUniqChar(String s) {
+            // 找到不重复的字符返回其索引。不存在则返回-1
+            // 使用hashmap记录字符出现的次数，如果次数为一直接返回
+            char[] chars = s.toCharArray();
+            Map<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < chars.length; i++) {
+                map.put(chars[i], map.getOrDefault(chars[i], 0) + 1);
             }
-        }
-        for (Character c : map.keySet()) {
-            if (map.get(c) == 1) {
-                for (int i = 0; i < chars.length; i++) {
-                    if (c == chars[i]) return i;
+            for (int i = 0; i < chars.length; i++) {
+                if (map.get(chars[i]) == 1) {
+                    return i;
                 }
             }
+            return -1;
         }
-        return -1;
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
