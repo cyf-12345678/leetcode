dee380107e8a94c72916b2e930d2e9ab6d05c772
chengyinfeng <itxc49001@gmail.com>
2022-01-06 19:50:53 +0800

    每日leetcode

---
+++ src/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -0,0 +1,84 @@
+package leetcode.editor.cn;//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+// Related Topics 递归 链表 👍 2129 👎 0
+
+//Java：合并两个有序链表
+public class MergeTwoSortedLists{
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+        // TO TEST
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode res;
+        if (list1.val < list2.val) {
+            res = list1;
+        } else {
+            res = list2;
+        }
+
+        ListNode newNode;
+        if (list1.val >= list2.val) {
+            newNode = list1;
+        } else {
+            newNode = list2;
+        }
+
+        res.next = mergeTwoLists(res.next, newNode);
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
